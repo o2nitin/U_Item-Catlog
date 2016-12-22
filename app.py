b18fa2bcf273c7575a55d0f2b675d9d4fd67bbe3
Nitin Kumar Singh <nitinksingh11@gmail.com>
2016-12-22 16:06:40 +0530

    basic routes setup

--- app.py
+++ app.py
@@ -9,6 +9,32 @@ def showItemss():
         return render_template('index.html')
 
 
+@app.route('/catalog/Snowboarding/items')
+def showAllItemss():
+    # return "This page will show all home page"
+        return render_template('items.html')
+
+@app.route('/catalog/Snowboarding/Snowboard')
+def showOneItemss():
+    # return "This page will show all home page"
+        return render_template('item.html')
+
+@app.route('/catalog/Snowboard/edit')
+def editItemss():
+    # return "This page will show all home page"
+        return render_template('edititem.html')
+
+@app.route('/catalog/Snowboard/delete')
+def deleteItemss():
+    # return "This page will show all home page"
+        return render_template('deleteitem.html')
+
+@app.route('/catalog.json')
+def showItemssApi():
+    # return "This page will show all home page"
+        return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5005)
