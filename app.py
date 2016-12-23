57e665dd8d64060fde501fa1c42ce76fd6602996
Nitin Kumar Singh <nitinksingh11@gmail.com>
2016-12-23 14:03:24 +0530

    view catogry and  items, url created

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
 
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 def showItemss():
     # return "This page will show all home page"
-        return render_template('index.html')
+        categories = session.query(Category).all()
+        return render_template('index.html',categories=categories)
+
+@app.route('/addnewcat', methods=['GET', 'POST'])
+def newCat():
+    if request.method == 'POST':
+        newCat = Category(name=request.form['name'],url=request.form['url'])
+        session.add(newCat)
+        session.commit()
+        return redirect(url_for('showItemss'))
+    else:
+        return render_template('addcat.html')
 
 
-@app.route('/catalog/Snowboarding/items')
-def showAllItemss():
+@app.route('/catalog/<int:id>/items')
+def showAllItemss(id):
+    category = session.query(Category).filter_by(id=id).one()
+    items = session.query(Item).filter_by(
+        category_id=id).all()
+    return render_template('items.html', items=items, category=category)
     # return "This page will show all home page"
-        return render_template('items.html')
+    
 
 @app.route('/catalog/Snowboarding/Snowboard')
 def showOneItemss():
@@ -37,4 +61,4 @@ def showItemssApi():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5005)
+    app.run(host='0.0.0.0', port=5000)
---
+++ database_setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    url = Column(String(250))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'url':self.url,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    img_url = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'img_url': self.price,
+        }
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+
+Base.metadata.create_all(engine)
